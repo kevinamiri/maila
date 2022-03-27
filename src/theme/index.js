f275d570fa6beae1b1622d81e6f23a60753a1f5a
kevin amiri <amiri990.us@gmail.com>
2022-03-27 18:28:07 +0200

    google captcha, change arrow icon

--- src/theme/index.js
+++ src/theme/index.js
@@ -8,6 +8,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import './styles.css'
 
 const neutral = {
   100: '#F3F4F6',
