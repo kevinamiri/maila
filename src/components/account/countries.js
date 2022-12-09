bffd0624982eec69db17e47fbe0e815df49328ee
kevin amiri <amiri990.us@gmail.com>
2022-12-09 12:07:33 +0100

    add

--- src/components/account/countries.js
+++ src/components/account/countries.js
@@ -1,18 +1,9 @@
 const countries = [
   { code: "US", label: "English", LangCode: "en" },
   { code: "SV", label: "Swedish", LangCode: "sv" },
-  { code: "DE", label: "German", LangCode: "de" },
+  { code: "DE", label: "German", LangCode: "da" },
   { code: "FI", label: "Finnish", LangCode: "fi" },
-  { code: "NO", label: "Norwegian", LangCode: "no" },
-  { code: "FR", label: "French", LangCode: "fr" },
-  { code: "IR", label: "Persian", LangCode: "fa" },
-  { code: "CN", label: "Chinese", LangCode: "zh" },
-  { code: "RU", label: "Russian", LangCode: "ru" },
-  { code: "NL", label: "Dutch", LangCode: "nl" },
-  { code: "KO", label: "Korean", LangCode: "ko" },
-  { code: "TR", label: "Turkish", LangCode: "tr" },
-  { code: "PT", label: "Portuguese", LangCode: "pt" },
-  { code: "ES", label: "Spanish", LangCode: "es" },
+  { code: "NO", label: "Norwegian", LangCode: "no" }
 ];
 
 export default countries;
