e2b298877b352eb7e9b10ceea65b3b27ec49570a
Kevin Amiri <amiri990.us@gmail.com>
2023-04-13 23:56:03 +0200

    add tools page

---
+++ src/pages/tools.en.md
@@ -0,0 +1,12 @@
+---
+id: "12"
+title: "List of templates"
+description: "All copywriting templates"
+templateKey: tools-lists
+date: "11-04-2023"
+slug: /en/tools
+path: /en/blog
+lang: en
+tags: ['templates', 'tools', 'use-cases']
+---
+# List of templates
---
+++ src/templates/tools-lists.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+import BlogLists from "../components/homepage/blog-lists";
+import { graphql } from "gatsby";
+
+const BlogListsPage = (props) => {
+  const data = props.data;
+  const location = props.location;
+  return (
+    <Layout data={data} location={location}>
+      <BlogLists data={data} />
+    </Layout>
+  );
+};
+
+export default BlogListsPage;
+
+export const pageQuery = graphql`
+  query BlogRollQuery($id: String, $lang: String) {
+    site {
+      siteMetadata {
+        title
+        languages {
+          langs
+          defaultLangKey
+        }
+      }
+    }
+    allMarkdownRemark(
+      sort: { frontmatter: { date: DESC } }
+      filter: {
+        frontmatter: { templateKey: { eq: "tools-body" } }
+        fields: { langKey: { eq: $lang } }
+      }
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 300)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date
+            lang
+            image {
+              childImageSharp {
+                gatsbyImageData(width: 500, layout: CONSTRAINED)
+              }
+            }
+            tags
+          }
+        }
+      }
+    }
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        id
+        title
+        image {
+          childImageSharp {
+            gatsbyImageData(width: 800, layout: CONSTRAINED)
+          }
+        }
+        description
+        date
+        tags
+        lang
+      }
+    }
+  }
+`;
